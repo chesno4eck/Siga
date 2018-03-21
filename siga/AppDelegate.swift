bce9643a0e18e3e9c0ba4a98c26335b53d9b5d56
Дмитрий Алиев <du.aliev@tensor.ru>
2018-03-21 20:32:11 +0300

    initial

---
+++ siga/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  siga
+//
+//  Created by Дмитрий Алиев on 21/03/2018.
+//  Copyright © 2018 Дмитрий Алиев. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
